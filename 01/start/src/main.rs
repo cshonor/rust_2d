81b2bae3d8338c05103d3128a7b7b1bef3cc8e26
wzp <1239263644@qq.com>
2025-10-18 00:13:12 +0800

    实现gamestate trait

--- 01/start/src/main.rs
+++ 01/start/src/main.rs
@@ -1,3 +1,14 @@
+use bracket_lib::prelude::*;
 fn main() {
-    println!("Hello, world!");
+    
+}
+
+struct state {
+   
+}
+impl GameState for state {
+    fn tick(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
+        ctx.print(1, 1, "Hello, world!");
+    }
 }
