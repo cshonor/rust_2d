e82fb3b2f74fcdad9b00f86f3a1e273bfa187cfd
wzp <1239263644@qq.com>
2025-10-20 06:22:28 +0800

    将 Rust 项目从 01/start 移动到 start 目录并更新 gitignore   完成menu

--- start/src/main.rs
+++ start/src/main.rs
@@ -1,6 +1,10 @@
 use bracket_lib::prelude::*;
 fn main() -> BError {
-    
+    let context=BTermBuilder::simple80x50().with_title("Flappy Dragon").build()?;
+    main_loop(context,State::new());
+    Ok(())
+}
+
 enum GameMode {
     Menu,
     Playing,
@@ -31,11 +35,26 @@ impl State {
 
     
     fn draw_menu(&mut self, ctx: &mut BTerm) {
+        ctx.cls();
         ctx.print(1, 1, "Welcome to Flappy Dragon");
-        ctx.print(1, 3, "Press SPACE to start");
+        ctx.print(1, 3, "Press P to start");
+        ctx.print(1, 5, "Press Q to quit");
+
+        if let Some(key)=ctx.key {
+            match key {
+                VirtualKeyCode::P => self.restart(),
+                VirtualKeyCode::Q => ctx.quitting = true,
+                _ => {}
+            }
+        }
     }
     fn draw_playing(&mut self, ctx: &mut BTerm) {
         ctx.print(1, 1, "Playing");
+        //todo
+        self.mode=GameMode::End;
+    }
+    fn restart(&mut self) {
+        self.mode=GameMode::Playing;
     }
     fn draw_end(&mut self,  ctx: &mut BTerm) {
         ctx.print(1, 1, "Game Over");
@@ -43,10 +62,3 @@ impl State {
 }
 
 
-let context=BTermBuilder::simple80x50().with_title("Flappy Dragon").build()?;
-
-
-main_loop(context,State：：new());
-
-
-}
