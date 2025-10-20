737aa3ab809d933c41a352d7fac10fc9f786c85b
wzp <1239263644@qq.com>
2025-10-20 20:01:05 +0800

    实现玩家

--- start/src/main.rs
+++ start/src/main.rs
@@ -61,4 +61,16 @@ impl State {
     }
 }
 
-
+struct Player {
+    x: i32,
+    y: i32,
+    velocity: i32,
+}
+impl Player {
+    fn new(x: i32, y: i32) -> Self {
+        Self { x, y, velocity: 0 }
+    }
+    fn render(&mut self, ctx: &mut BTerm) {
+        ctx.set(0, self.y, YELLOW, BLACK,to_cp437('@'));
+    }
+}
